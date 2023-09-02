5b61e90eb557698a356cdaaf1b076eea5a8eddbb
Maddi Cook <maddihcook@gmail.com>
2023-09-02 11:16:22 -0500

    finished authentication

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -4,23 +4,24 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { registerUser } from '../utils/auth'; // Update with path to registerUser
 
-function RegisterForm({ user, updateUser }) {
+function RegisterForm({ user, onUpdate }) {
   const [formData, setFormData] = useState({
     bio: '',
-    uid: user.uid,
+    customerId: user.uid,
+    isSeller: false,
   });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    registerUser(formData).then(() => updateUser(user.uid));
+  const handleSubmit = () => {
+    registerUser(formData);
+    onUpdate();
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>Name</Form.Label>
+        <Form.Control as="textarea" name="name" required placeholder="Name" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        {/* <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text> */}
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
@@ -33,7 +34,7 @@ RegisterForm.propTypes = {
   user: PropTypes.shape({
     uid: PropTypes.string.isRequired,
   }).isRequired,
-  updateUser: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default RegisterForm;
